2b4a39397ce7a0c9fb86f99c69c7e4ea75f7d5c4
Eraclio Vega <eracliovega@Eraclios-MBP.attlocal.net>
2023-12-11 15:53:35 -0500

    set up

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import "./App.css";
+
+// Import components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+
+// Import pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,6 @@
+function Projects(props) {
+    return <h1>Projects</h1>;
+  }
+  
+  export default Projects;
+  
